ae1d1783940e28eca0cfa7c617aaf92a1d2b123b
raissalunana <raissalunana@gmail.com>
2023-09-27 22:34:38 -0300

    botão finalizado

---
+++ 2-Plantcard/main.js
@@ -0,0 +1,14 @@
+let button = document.querySelector('div div div .btnn');
+let criacao = document.querySelector('div div div .share');
+function abrirmodal(){
+    if (criacao.style.visibility === 'hidden') {
+        criacao.style.visibility = 'visible';
+    }
+    else{
+        criacao.style.visibility = 'hidden';
+    }
+}
+
+
+
+/* para ocular o que vai aparecer, e o que vai aparecer? a outra div com várias paradas*/
