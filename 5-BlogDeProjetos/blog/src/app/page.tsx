0895dcce8d634b4d31c44ab6af3408416068a8d5
raissalunana <raissalunana@gmail.com>
2023-10-19 02:46:11 -0300

    Inicio do projeto: atualizando a estilização > configurando fontes

---
+++ 5-BlogDeProjetos/blog/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1 className="text-5xl font-display font-bold">hello world!</h1>
+    </main>
+  )
+}
