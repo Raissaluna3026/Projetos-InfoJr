4a2c7e8e5752ef656cfa5cf4e690b77b4961584b
raissalunana <raissalunana@gmail.com>
2023-10-23 14:29:31 -0300

    faltando apenas pegar usuario por email e um ou mais por nome

---
+++ 6-CrudMembros/src/controllers/products.ts
@@ -0,0 +1,129 @@
+import { Request, Response} from 'express';
+import { badRequest, internalServerError, notFound, ok, validateNumber } from '../services/util';
+import { Product, productModel } from '../models/products';
+
+
+const insertProduct = (req: Request, res: Response) => {
+    
+    {
+        const product = req.body
+        if (!product)
+            return badRequest(res, "Produto inválido");
+
+        if (!product.name)
+            return badRequest(res, "Informe o nome");
+
+        if (!product.idade)
+            return badRequest(res, "Informe a idade");
+
+        if (!product.email)
+            return badRequest(res, "Informe o email");
+        
+        if (!product.estado)
+            return badRequest(res, "Informe o estado");
+        
+        if (!product.cidade)
+            return badRequest(res, "Informe a cidade");
+    }   
+
+    const product = req.body as Product;
+    return productModel.insertProduct(product)
+        .then(product => {
+            res.json(product)
+        })
+        .catch(err=> internalServerError(res, err))
+}
+
+const updateProduct = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id)
+
+    {   
+        if(!validateNumber(id))
+            return badRequest(res,"id inválido")
+        const product = req.body
+        if (!product)
+            return badRequest(res, "Produto inválido");
+
+        if (!product.name)
+            return badRequest(res, "Informe o nome");
+
+        if (!product.idade)
+            return badRequest(res, "Informe a idade");
+
+        if (!product.email)
+            return badRequest(res, "Informe o email");
+        
+        if (!product.estado)
+            return badRequest(res, "Informe o estado");
+        
+        if (!product.cidade)
+            return badRequest(res, "Informe a cidade");
+
+        const productSaved = await productModel.getProduct(id)
+        if (!productSaved)
+            return notFound(res)
+
+    }   
+
+    const product = req.body as Product;
+    return productModel.updateProduct(product)
+        .then(product => {
+            res.json(product)
+        })
+        .catch(err=> internalServerError(res, err))
+}
+
+const listProducts = ({}: Request, res: Response) => {
+    productModel.listProducts()
+        .then(products => {
+            res.json(products)
+        })
+        .catch(err=> internalServerError(res, err))
+}
+
+const getProduct = (req: Request, res: Response) => {
+    const id = parseInt(req.params.id)
+    {
+        if(!validateNumber(id))
+            return badRequest(res,"id inválido")
+
+        
+    }
+
+
+    return productModel.getProduct(id)
+        .then((product) => {
+            if(product)
+                return res.json(product)
+            else
+                return notFound(res)
+        })
+        .catch(err=> internalServerError(res, err))
+}
+
+const deleteProduct = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id)
+    {
+        if(!validateNumber(id))
+            return badRequest(res,"id inválido")
+
+        const productSaved = await productModel.getProduct(id)
+        if (!productSaved)
+            return notFound(res)
+    }
+
+
+    return productModel.deleteProduct(id)
+        .then(() => ok(res))
+        .catch(err=> internalServerError(res, err))
+}
+
+
+
+export const productController = {
+    insertProduct,
+    listProducts,
+    getProduct,
+    deleteProduct,
+    updateProduct
+}
---
+++ 6-CrudMembros/src/models/products.ts
@@ -0,0 +1,47 @@
+import { dbQuery, dbQueryFirst } from "../services/db";
+
+export type Product = {
+    id: number;
+    name: string;
+    email: string;
+    idade: number;
+    estado: string;
+    cidade: string;
+}
+//inserir
+const insertProduct = async (product: Product) => {
+    await dbQuery('INSERT INTO product (name, idade, email, cidade, estado) VALUES(?,?,?,?,?)' , [product.name, product.idade, product.email, product.cidade, product.estado])
+    let retorno = await dbQuery("SELECT seq AS Id FROM sqlite_sequence WHERE name = 'product'")
+    return  getProduct(retorno[0].Id);
+}
+//update
+const updateProduct = async (product: Product) => {
+    await dbQuery('UPDATE product SET name = ?, idade = ?, email = ?, cidade =?, estado =? WHERE id =?', [product.name, product.idade, product.email, product.cidade, product.estado, product.id])
+    return getProduct(product.id)
+    
+}
+//selecionar
+const listProducts = async () => {
+    const retorno = await dbQuery('SELECT * FROM product');
+    return retorno as Product[];
+
+
+}
+//buscar por id
+const getProduct = async (id: number) => {
+    const retorno = await dbQueryFirst('SELECT * FROM product WHERE id = ?', [id]);
+    return retorno as Product | undefined;
+}
+
+//deletar
+const deleteProduct = async (id: number) => {
+    const retorno = await dbQueryFirst('DELETE FROM product WHERE id = ?', [id]);
+}
+
+export const productModel = {
+    insertProduct,
+    listProducts,
+    getProduct,
+    deleteProduct,
+    updateProduct
+}
