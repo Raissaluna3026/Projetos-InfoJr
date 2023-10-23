9fcff92e5c446225e5768097b36e07ed9fded192
raissalunana <raissalunana@gmail.com>
2023-10-23 16:53:49 -0300

    recuperando e recomeçando

---
+++ 6-CrudMembros/src/controllers/products.ts
@@ -0,0 +1,155 @@
+import { Request, Response} from 'express';
+import { badRequest, internalServerError, notFound, ok, validateNumber } from '../services/util';
+import { Product, productModel } from '../models/products';
+
+
+const insertProduct = (req: Request, res: Response) => {
+    
+    {
+        const product = req.body
+        if (!product)
+            return badRequest(res, "Produto inválido");
+
+        if (!product.name)
+            return badRequest(res, "Informe o nome");
+
+        if (!product.idade)
+            return badRequest(res, "Informe a idade");
+
+        if (!product.email)
+            return badRequest(res, "Informe o email");
+        
+        if (!product.estado)
+            return badRequest(res, "Informe o estado");
+        
+        if (!product.cidade)
+            return badRequest(res, "Informe a cidade");
+    }   
+
+    const product = req.body as Product;
+    return productModel.insertProduct(product)
+        .then(product => {
+            res.json(product)
+        })
+        .catch(err=> internalServerError(res, err))
+}
+
+const updateProduct = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id)
+
+    {   
+        if(!validateNumber(id))
+            return badRequest(res,"id inválido")
+        const product = req.body
+        if (!product)
+            return badRequest(res, "Produto inválido");
+
+        if (!product.name)
+            return badRequest(res, "Informe o nome");
+
+        if (!product.idade)
+            return badRequest(res, "Informe a idade");
+
+        if (!product.email)
+            return badRequest(res, "Informe o email");
+        
+        if (!product.estado)
+            return badRequest(res, "Informe o estado");
+        
+        if (!product.cidade)
+            return badRequest(res, "Informe a cidade");
+
+        const productSaved = await productModel.getProduct(id)
+        if (!productSaved)
+            return notFound(res)
+
+    }   
+
+    const product = req.body as Product;
+    return productModel.updateProduct(product)
+        .then(product => {
+            res.json(product)
+        })
+        .catch(err=> internalServerError(res, err))
+}
+
+const listProducts = ({}: Request, res: Response) => {
+    productModel.listProducts()
+        .then(products => {
+            res.json(products)
+        })
+        .catch(err=> internalServerError(res, err))
+}
+
+const getProduct = (req: Request, res: Response) => {
+    const id = parseInt(req.params.id)
+    {
+        if(!validateNumber(id))
+            return badRequest(res,"id inválido")  
+    }
+
+    return productModel.getProduct(id)
+        .then((product) => {
+            if(product)
+                return res.json(product)
+            else
+                return notFound(res)
+        })
+        .catch(err=> internalServerError(res, err))
+}
+
+const getProductByEmail = (req: Request, res: Response) => {
+    const email = req.params.email; 
+
+    return productModel.getProductByEmail(email)
+        .then((product) => {
+            if (product) {
+                return res.json(product);
+            } else {
+                return notFound(res);
+            }
+        })
+        .catch((err) => internalServerError(res, err));
+};
+
+const getProductByName = (req: Request, res: Response) => {
+    const name = req.params.name;
+
+    return productModel.getProductByName(name)
+        .then((product) => {
+            if(product.length > 0)
+                return res.json(product)
+            else
+                return notFound(res)
+        })
+        .catch(err=> internalServerError(res, err))
+}
+
+const deleteProduct = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id)
+    {
+        if(!validateNumber(id))
+            return badRequest(res,"id inválido")
+
+        const productSaved = await productModel.getProduct(id)
+        if (!productSaved)
+            return notFound(res)
+    }
+
+
+    return productModel.deleteProduct(id)
+        .then(() => ok(res))
+        .catch(err=> internalServerError(res, err))
+}
+
+
+
+export const productController = {
+    insertProduct,
+    listProducts,
+    getProduct,
+    deleteProduct,
+    updateProduct,
+    getProductByEmail,
+    getProductByName
+}
---
+++ 6-CrudMembros/src/routes/products.ts
@@ -0,0 +1,17 @@
+import { Router,Request,Response  } from "express"
+import { productController } from "../controllers/products";
+
+const productRouter = Router();
+productRouter.post('/', productController.insertProduct)
+productRouter.get('/', productController.listProducts)
+productRouter.get('/:id', productController.getProduct)
+productRouter.get('/email/:email', productController.getProductByEmail);
+productRouter.get('/name/:name', productController.getProductByName);
+productRouter.delete('/:id', productController.deleteProduct)
+productRouter.put('/:id', productController.updateProduct)
+
+
+export {
+    productRouter
+}
+
